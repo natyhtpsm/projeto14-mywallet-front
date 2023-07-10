93840c9c58f97ec60e779f36a44944bc37367a3f
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-10 03:45:54 -0300

    feat: signup post

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,19 +1,57 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import dotenv from 'dotenv';
+
+dotenv.config();
+const url = process.env.DATABASE_URL + '/cadastro';
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkpassword, setcheckpassword] = useState('');
+  const [name, setName] = useState('');
+  
+  function sendData() {
+    const data = {
+      email: email,
+      name: name,
+      password: password, 
+      checkpassword: checkpassword
+    };
+    if (password === checkpassword){
+      axios
+      .post(url, data)
+      .then(response => {
+        const statusCode = response.status;
+        if (statusCode === 422) {
+          alert('O email deve ter formato válido e a senha deverá conter no mínimo 3 dígitos');
+        }
+        if (statusCode === 409){
+          alert('Esse email já foi cadastrado');
+        }
+        navigate('/');
+      })
+      .catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+      });
+    }
+    
+  }
   return (
     <SingUpContainer>
       <form>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" type="text" onChange={(e) => setName(e.target.value)}/>
+        <input placeholder="E-mail" type="email" onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" autocomplete="new-password" onChange={(e) => setPassword(e.target.value)}/>
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" onChange={(e) => setcheckpassword(e.target.value)} />
+        <button onClick={sendData}>Cadastrar</button>
       </form>
-
       <Link>
         Já tem uma conta? Entre agora!
       </Link>
